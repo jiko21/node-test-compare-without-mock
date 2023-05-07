044f3ca2935fb2981f69dd71e995f7c0ee8d242c
jiko21 <daikikojima0531@icloud.com>
2023-05-07 15:07:00 +0900

    remove: mock

--- src/mock.test.js
+++ src/mock.test.js
@@ -1,8 +1,6 @@
 const { describe, test } = require('node:test');
 const assert = require('node:assert/strict');
 const { someFunc1 } = require('./mock');
-const { mockFn } = require('./mockfn');
-const mocked = require('./mocked');
 
 describe('mock sample', () => {
   test('mock already existing object method', () => {
