c2bfa5979cc8fb77ac952d3db45841c011ba19a8
jiko21 <daikikojima0531@icloud.com>
2023-05-09 00:49:53 +0900

    fix: command

--- src/mock.test.js
+++ src/mock.test.js
@@ -1,9 +1,8 @@
-const { describe, test } = require('node:test');
 const assert = require('node:assert/strict');
 const { someFunc1 } = require('./mock');
 
 describe('mock sample', () => {
   test('mock already existing object method', () => {
-    assert.deepEqual(someFunc1(), 3);
+    expect(someFunc1()).toBe(3);
   });
 });
